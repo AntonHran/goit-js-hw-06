51c0223ff4e79b0c04993079b9f8158ed6208b93
AntonHran <for.program.learning@gmail.com>
2024-04-18 23:31:57 +0300

    edited

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listEl = document.querySelector("#categories");
+const list = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${list.length}`);
+const res = list.forEach((el) => {
+  console.log(el.classList.);
+});
