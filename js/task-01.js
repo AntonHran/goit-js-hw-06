f7959e40f2d8f5d9c06841bcfc02ad76a188ecaf
AntonHran <for.program.learning@gmail.com>
2024-04-19 00:03:26 +0300

    task1 was done

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,10 @@
 const listEl = document.querySelector("#categories");
+//console.log(listEl.children);
 const list = document.querySelectorAll(".item");
 
-console.log(`Number of categories: ${list.length}`);
+console.log(`Number of categories: ${listEl.children.length}`);
 const res = list.forEach((el) => {
-  console.log(el.classList.);
+  console.log(el.firstElementChild.textContent);
+  console.log(el.lastElementChild.childElementCount);
+  //console.log(el.lastElementChild.children.length);
 });
