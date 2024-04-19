6bacefd7126c70b1f23d428f237e6dbbe7c7444a
AntonHran <for.program.learning@gmail.com>
2024-04-19 23:11:52 +0300

    task 2 was done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+const makeListElements = (list) => {
+  return list.map((element) => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("item");
+    liEl.textContent = element;
+    return liEl;
+  });
+};
+
+const liElements = makeListElements(ingredients);
+ulEl.append(...liElements);
