84c52849e2b55ef1e9d85f79aa3e5affbd306269
AntonHran <for.program.learning@gmail.com>
2024-04-21 14:58:57 +0300

    task 9 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const registerForm = document.querySelector(".login-form");
 
+const userObj = {
+  email: "",
+  password: "",
+};
+
+function fillObject(emailVal, passwordVal) {
+  userObj.email = emailVal;
+  userObj.password = passwordVal;
+  return userObj;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (!email || !password) {
+    alert("Some field(-s) are not filled!!!");
+  } else {
+    console.log(fillObject(email, password));
+  }
+  form.reset();
+}
+
+registerForm.addEventListener("submit", handleSubmit);
